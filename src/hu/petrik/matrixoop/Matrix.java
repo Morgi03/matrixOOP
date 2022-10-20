ccd807ab062cd9ec87f4f0f0d72894b877e34973
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-20 08:38:10 +0200

    Mátrix megírása és tesztelése

--- src/hu/petrik/matrixoop/Matrix.java
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -7,4 +7,29 @@ public class Matrix {
     private int oszlop;
     private int[][] m;
     private static final Random rnd = new Random();
+
+    public Matrix(int sor, int oszlop) {
+        this.sor = sor;
+        this.oszlop = oszlop;
+        this.m = new int[sor][oszlop];
+        for (int i = 0; i < this.m.length; i++) {
+            for (int j = 0; j < this.m[i].length; j++) {
+                this.m[i][j] = rnd.nextInt(1000);
+            }
+        }
+        // this.m.length = sor
+        // this.m[i].length = oszlop
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+            for (int[] sor: this.m) {
+                for (int elem: sor) {
+                    sb.append(String.format("%3d ",elem));
+                }
+                sb.append(System.lineSeparator());
+        }
+        return sb.toString().trim();
+    }
 }
