4be9aa4b92076eb54449650809e6b81b8139b9fe
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-20 08:23:47 +0200

    Initial commit

---
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -0,0 +1,10 @@
+package hu.petrik.matrixoop;
+
+import java.util.Random;
+
+public class Matrix {
+    private int sor;
+    private int oszlop;
+    private int[][] m;
+    private static final Random rnd = new Random();
+}
